b9ca2864d0464c8ef1d8092cad45108d3bba8677
Iryna Finenko <irynafinenko@gmail.com>
2025-07-10 16:13:42 +0300

    fixed probblem after review

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,5 +1,6 @@
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (password == null || !password.equals(repeatPassword)
                 || password.length() < 10) {
             throw new PasswordValidationException("Wrong passwords");
